5ca6b054b617c212a8651115ca69fad334bc3880
besiak6 <besi4k6@gmail.com>
2025-07-28 23:33:47 +0200

    Create baddonz.user.js

---
+++ baddonz.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name          Baddonz
+// @version       0.3
+// @description   Nowy menadżer dodatków by besiak
+// @author        besiak
+// @match         https://*.margonem.pl/*
+// @grant         none
+// @icon          https://i.imgur.com/OAtRFEw.png
+// @downloadURL   https://raw.githubusercontent.com/besiak6/baddonz/refs/heads/main/baddonz.user.js
+// @updateURL     https://raw.githubusercontent.com/besiak6/baddonz/refs/heads/main/baddonz.user.js
+// ==/UserScript==
+(function() {
+    window.CSS_URL = "https://raw.githubusercontent.com/besiak6/baddonz/refs/heads/main/besiak.css";
+    window.ADDON_VERSION = "0.3";
+    const version = Date.now();
+    const build = "https://addons2.margonem.pl/get/153/153736dev.js";
+    const script = document.createElement("script");
+    script.src = `${build}?v=${version}`;
+    document.body.appendChild(script);
+})();
